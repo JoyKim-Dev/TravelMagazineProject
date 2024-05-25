2aa50434d60a5e3308415f9ac4bdb61a947e7075
Joy <kimjoydev@gmail.com>
2024-05-25 23:58:10 +0900

    Initial Commit

---
+++ TravelMagazineProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelMagazineProject
+//
+//  Created by Joy Kim on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
