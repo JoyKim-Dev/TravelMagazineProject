18f863045c955f16da7069c43e86fb8ecc7c9532
Joy <kimjoydev@gmail.com>
2024-05-27 03:11:27 +0900

    Chore: 나중에 사용할 viewController 내용 주석처리

--- TravelMagazineProject/TravelMain/TravelMainViewController.swift
+++ TravelMagazineProject/TravelMain/TravelMainViewController.swift
@@ -1,63 +1,63 @@
+////
+////  TravelMainViewController.swift
+////  TravelMagazineProject
+////
+////  Created by Joy Kim on 5/26/24.
+//탭맨으로 상단 탭바 구현 to be continued...일단 네비게이션바로 만들었음.
 //
-//  TravelMainViewController.swift
-//  TravelMagazineProject
+//import UIKit
+//import Tabman
+//import Pageboy
 //
-//  Created by Joy Kim on 5/26/24.
+//class TravelMainViewController: TabmanViewController {
+//    
+//    private var viewControllers: Array<UIViewController> = []
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let travelVc = UIStoryboard.init(name: "TravelMain", bundle: nil).instantiateViewController(withIdentifier: "TravelMainViewController") as! TravelMainViewController
+//        let restaurantVc = UIStoryboard.init(name: "TravelMain", bundle: nil).instantiateViewController(withIdentifier: "RestaurantViewController") as! RestaurantViewController
+//        
+//        viewControllers.append(travelVc)
+//        viewControllers.append(restaurantVc)
+//        
+//        self.dataSource = self
+//        
+//        // Create bar
+//        let bar = TMBar.ButtonBar()
+//        bar.layout.transitionStyle = .snap // Customize
+//        
+//        // Add to view
+//        addBar(bar, dataSource: self, at: .top)
+//    }
+//       
+//}
 //
-
-import UIKit
-import Tabman
-import Pageboy
-
-class TravelMainViewController: TabmanViewController {
-    
-    private var viewControllers: Array<UIViewController> = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let travelVc = UIStoryboard.init(name: "TravelMain", bundle: nil).instantiateViewController(withIdentifier: "TravelMainViewController") as! TravelMainViewController
-        let restaurantVc = UIStoryboard.init(name: "TravelMain", bundle: nil).instantiateViewController(withIdentifier: "RestaurantViewController") as! RestaurantViewController
-        
-        viewControllers.append(travelVc)
-        viewControllers.append(restaurantVc)
-        
-        self.dataSource = self
-        
-        // Create bar
-        let bar = TMBar.ButtonBar()
-        bar.layout.transitionStyle = .snap // Customize
-        
-        // Add to view
-        addBar(bar, dataSource: self, at: .top)
-    }
-       
-}
-
-extension TravelMainViewController: PageboyViewControllerDataSource, TMBarDataSource {
-    
-    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
-        let item = TMBarItem(title: "")
-        item.title = "Page \(index)"
-        item.image = UIImage(named: "image.png")
-        // ↑↑ 이미지는 이따가 탭바 형식으로 보여줄 때 사용할 것이니 "이미지가 왜 있지?" 하지말고 넘어가주세요.
-        
-        return item
-    }
-    
-    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
-        return viewControllers.count
-    }
-
-    func viewController(for pageboyViewController: PageboyViewController,
-                        at index: PageboyViewController.PageIndex) -> UIViewController? {
-        return viewControllers[index]
-    }
-
-    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
-        return nil
-    }
-}
-
-
-
+//extension TravelMainViewController: PageboyViewControllerDataSource, TMBarDataSource {
+//    
+//    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+//        let item = TMBarItem(title: "")
+//        item.title = "Page \(index)"
+//        item.image = UIImage(named: "image.png")
+//        // ↑↑ 이미지는 이따가 탭바 형식으로 보여줄 때 사용할 것이니 "이미지가 왜 있지?" 하지말고 넘어가주세요.
+//        
+//        return item
+//    }
+//    
+//    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+//        return viewControllers.count
+//    }
+//
+//    func viewController(for pageboyViewController: PageboyViewController,
+//                        at index: PageboyViewController.PageIndex) -> UIViewController? {
+//        return viewControllers[index]
+//    }
+//
+//    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+//        return nil
+//    }
+//}
+//
+//
+//
